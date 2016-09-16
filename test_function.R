201b87a69462c8a449b0ee697d15f306bdec9361
Dustin Herrmann <Dustin Herrmann>
2016-09-16 14:16:04 -0400

    Shiny code

--- test_function.R
+++ test_function.R
@@ -2,16 +2,26 @@
 
 library(aqp)
 library(lattice)
+library(shiny)
+full <- read.csv("//Aa.ad.epa.gov/ord/CIN/Users/main/L-P/LSchifma/Net MyDocuments/PROJECTS/Multicity Soil Analysis/Full1_InclPredictedTexture.csv")
 
-full <- read.csv("Full2.csv")
-depths(full) <- SampleSite_ID ~ UpperDepth_cm + LowerDepth_cm
+
+depths(full) <- SampleSite_ID ~ top + bottom
 site(full) <- ~ City_Name
+
+
+in1 <- selectInput("pick_attribute",
+                   label = "Pick a Soil Attribute",
+                   choices = names(full@horizons)[31:56], selected="Percent_Sand")
+
+
+out2 <- plotOutput("city_plots", height = "600px") 
+
 
 # Function to plot the variable varname from profile soilprof
 #  rows and cols for lattice plot layout
-city_plot <- function(soilprof, varname, rows, cols) {
-  soilprof@horizons$var <- soilprof@horizons[[varname]]
-  agg <- slab(soilprof, fm = City_Name ~ var)
+city_plot <- function(varname, rows, cols) {
+  agg <- slab(full, fm = as.formula(paste("City_Name ~", varname)))
   xyplot(top ~ p.q50 | City_Name, data=agg, ylab='Depth',
          xlab='median bounded by 25th and 75th percentiles',
          lower=agg$p.q25, upper=agg$p.q75, ylim=c(500,-2),
@@ -25,5 +35,18 @@ city_plot <- function(soilprof, varname, rows, cols) {
   )  
 }
 
-city_plot(full, "Percent_Sand", 3, 4)
+
+
+
+server <- function(input, output) {
+  output[["city_plots"]] <- renderPlot(
+    city_plot(input[["pick_attribute"]], 3, 4)
+  )
+}
+
+
+tab <- tabPanel("Soils Data",in1, out2)
+ui <- navbarPage("Urban Soils Assessment", tab)
+shinyApp(ui = ui, server = server)
+
 
